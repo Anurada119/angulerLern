c247093eb0af55fc15d537178d55958d96c33430
Anurada Gunarathna <anurada119@gmail.com>
2017-09-01 08:53:02 +0530

    conflicts fixed

--- app/js/sidenav.js
+++ app/js/sidenav.js
@@ -1,28 +1,39 @@
-$(document).ready(function () {
-    $('.mp-burger').on('click', function () {
+
+$(document).ready(function() {
+    $('.mp-burger').on('click', function() {
         $('.mp-sidenav').toggleClass('sidenav-open');
         $('.mp-burger').toggleClass('sidenav-open');
         $('.mp-grid').toggleClass('sidenav-open');
+        $('.mp-quote-slider').toggleClass('menu-fix');
         $('.mp-cv-link svg').toggle();
 
         if ($('mp-grid').hasClass('sidenav-open')) {
-            $('.mp-wrapper').css({
-                'overflow': 'hidden'
-            })
+            $('.main-page').removeAttr("style");
+            $('.mp-burger').removeClass('removeMe');
+            alert();
         }
 
-        $('.mp-main-nav li').on('click', function () {
+        $('.mp-main-nav li').on('click', function() {
             $('.mp-sidenav').removeClass('sidenav-open');
             $('.mp-cv-link svg').toggle();
             $('.mp-burger').removeClass('sidenav-open');
+            $('.mp-quote-slider').removeClass('menu-fix');
         })
     });
 
-    if ($('#main').hasClass('post-page')) {
-        $('.mp-quote-slider').hide();
-    }
+    //sidenavHeight()
 
-    
+
+
+    $('.mp-burger').on('click', function() {
+        var sideNavHeight = $('.mp-sidenav').height();
+        var wHeight = $('.main-page').offset().top;
+        // $('.main-page').css({
+        //     'height': sideNavHeight - wHeight + 'px'
+        // });
+        // $('.main-page').css('height', 'auto');
+
+
+    });
 });
-
 
